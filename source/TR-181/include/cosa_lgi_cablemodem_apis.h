074791c23f79beb54e1eae19312da86c6e2df58e
Anu Chacko <achacko.contractor@libertyglobal.com>
2020-07-16 18:11:46 +0530

    Mercury V2 Connect Cloud support - Cable Modem Parameters ( RDKB-4477 : CR015 )
    Description:
    - Added TR-181 DML parameters of X_LGI-COM_CableModem object
    - Added implementation of X_LGI-COM_CableModem parameters
     - Device.X_LGI-COM_CableModem.CMStatus
     - Device.X_LGI-COM_CableModem.ToDStatus
     - Device.X_LGI-COM_CableModem.CmDoc30SwRegistrationState
     - Device.X_LGI-COM_CableModem.T1Timeouts
     - Device.X_LGI-COM_CableModem.SoftwareOperStatus
     - Device.X_LGI-COM_CableModem.EnergyMgtOperStatus
     - Device.X_LGI-COM_CableModem.OvertempProtModeState
     - Device.X_LGI-COM_CableModem.PartServiceFallback20
     - Device.X_LGI-COM_CableModem.DownstreamPrimaryChannelID
     - Device.X_LGI-COM_CableModem.QosServiceFlow.UpstreamNumberOfEntries
     - Device.X_LGI-COM_CableModem.QosServiceFlow.DownstreamNumberOfEntries
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.QosParamSetNumberOfEntries
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.QosParamSet.{i}.MaxTrafficRate
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.QosParamSet.{i}.MaxTrafficBurst
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.QosParamSet.{i}.MinReservedRate
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.QosParamSet.{i}.MaxConcatBurst
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.QosParamSet.{i}.SchedulingType
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.QosParamSetNumberOfEntries
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.QosParamSet.{i}.MaxTrafficRate
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.QosParamSet.{i}.MaxTrafficBurst
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.QosParamSet.{i}.MinReservedRate
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.QosParamSet.{i}.MaxConcatBurst
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.QosParamSet.{i}.SchedulingType
    
    TESTING PERFORMED:
    
    - Verified CM operating status, ToD status, Energy management mode with ACS
    - Verified whether the CM is currently operating in over temperature protected mode with ACS
    - Verified partial service fallback enable/disable status with ACS
    - Verified CM software operation status with ACS
    - Verified QoS service flow details with ACS
    - Verified CM registration status with ACS
    - Verified CM T1 timeout with ACS
    - Verified Downstream primary channel ID with ACS
    - Verified X_LGI-COM_CableModem object using dmcli
    
    Signed-off-by: XXXX <XXXX@YYYY.com>
    
    drop dependency on Puma 6 specific status.h header
    
    Temporary solution. Code added to ccsp-cm-agent is currently
    bypassing the CH HAL abstraction and using Intel specific types (and
    functions exported by the HAL but not defined in the HAL API). More
    cleanup is required, but this should be enough to at least build the
    code without a direct dependency on the Puma 6 SDK.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>
    
    update CosaDmlGiGetCmDoc30SwRegistrationState() to reflect recent cm_hal changes
    
    The docsis_getCmDoc30SwRegistrationState() CM HAL function now
    returns the correct registration state directly so the previous
    mapping can be removed.
    
    Include cm_hal.h in cosa_lgi_cablemodem_apis.c to define CmRegState_t
    (it should have been included before, but missing it has only caused
    compiler warnings until now).
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>
    
    check CM HAL function status against the correct definition
    
    CM HAL functions return results of RETURN_OK and RETURN_ERR. Check
    for those specific values, rather than making the assumption that
    they map directly to other similar definitions such as STATUS_OK or
    ANSC_STATUS_SUCCESS.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

---
+++ source/TR-181/include/cosa_lgi_cablemodem_apis.h
@@ -0,0 +1,141 @@
+/*********************************************************************
+ * Copyright 2017-2019 ARRIS Enterprises, LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **********************************************************************/
+
+#ifndef  _COSA_LGI_CABLEMODEM_APIS_H
+#define  _COSA_LGI_CABLEMODEM_APIS_H
+
+#include "../middle_layer_src/cosa_apis.h"
+
+#define DOWNSTREAM_INDEX 1
+#define UPSTREAM_INDEX   2
+
+typedef enum
+{
+    UNESTABLISHED = 1,
+    ESTABLISHED
+}TodStatus_e;
+
+typedef struct cm_ds_qos_sflow_t {
+    int32_t FlowSID;
+    int32_t FlowDirection;
+} cm_ds_qos_sflow_t;
+
+typedef struct cm_ds_qos_paramset_t {
+    int32_t SFID;
+    int32_t MaxTrafficRate;
+    int32_t MaxTrafficBurst;
+    int32_t MinReservedRate;
+    int32_t MaxConcatBurst;
+    int32_t SchedulingType;
+} cm_ds_qos_paramset_t;
+
+/* Used to get Service Flow and Paramset for both upstream and downstream */
+typedef struct cm_lgi_qos_sflow_t {
+    int32_t FlowSID;
+    int32_t FlowDirection;
+} cm_lgi_qos_sflow_t;
+
+typedef struct cm_lgi_qos_paramset_t {
+    int32_t SFID;
+    int32_t MaxTrafficRate;
+    int32_t MaxTrafficBurst;
+    int32_t MinReservedRate;
+    int32_t MaxConcatBurst;
+    int32_t SchedulingType;
+} cm_lgi_qos_paramset_t;
+
+/**********************************************************************
+                FUNCTION PROTOTYPES
+**********************************************************************/
+
+ANSC_STATUS
+CosaDmlGiGetCmDoc30SwRegistrationState
+(
+    ANSC_HANDLE                 hContext,
+    ULONG                       *pValue
+);
+
+ANSC_STATUS
+CosaDmlGiGetT1Timeouts
+(
+    ANSC_HANDLE                 hContext,
+    ULONG                       *pValue
+);
+
+ANSC_STATUS
+CosaDmlGiGetHorizOvertempProtModeState
+(
+    ANSC_HANDLE                 hContext,
+    ULONG                       *pValue
+);
+
+ANSC_STATUS
+CosaDmlGiGetLgiCMStatus
+(
+    ANSC_HANDLE                 hContext,
+    ULONG                       *pValue
+);
+
+ANSC_STATUS
+CosaDmlGiGetLgiSoftwareOperStatus
+(
+    ANSC_HANDLE                 hContext,
+    ULONG                       *pValue
+);
+
+ANSC_STATUS
+CosaDmlGiGetLgiToDStatus
+(
+    ANSC_HANDLE                 hContext,
+    ULONG                       *pValue
+);
+
+ANSC_STATUS
+CosaDmlGiGetLgiEnergyMgtOperStatus
+(
+    ANSC_HANDLE                 hContext,
+    ULONG                       *pValue
+);
+
+ANSC_STATUS
+CosaDmlGiGetPartServiceFallback20
+(
+    ANSC_HANDLE                 hContext,
+    ULONG                       *pValue
+);
+
+ANSC_STATUS 
+CosaDmlGiGetQosServiceflow
+(
+    int serviceFlowCount, 
+    cm_lgi_qos_sflow_t **serviceFlowChannel
+);
+
+ANSC_STATUS CosaDmlGiGetQosServiceflowParamset
+(
+    int serviceFlowCount,
+    cm_lgi_qos_sflow_t **serviceFlowChannel,
+    cm_lgi_qos_paramset_t **serviceFlowParamset
+);
+
+ANSC_STATUS
+CosaDmlGiGetLgiDownstreamPrimaryChannelID
+(
+    ANSC_HANDLE                 hContext,
+    ULONG                       *pValue
+);
+#endif
+
