a6f897c9e6471d4e921d4e8bcdfe7bb224d19f33
Anu Chacko <achacko.contractor@libertyglobal.com>
2020-07-16 18:11:46 +0530

    Mercury V2 Connect Cloud support - Cable Modem Parameters ( RDKB-4477 : CR015 )
    Description:
    - Added TR-181 DML parameters of X_LGI-COM_CableModem object
    - Added implementation of X_LGI-COM_CableModem parameters
     - Device.X_LGI-COM_CableModem.CMStatus
     - Device.X_LGI-COM_CableModem.ToDStatus
     - Device.X_LGI-COM_CableModem.CmDoc30SwRegistrationState
     - Device.X_LGI-COM_CableModem.T1Timeouts
     - Device.X_LGI-COM_CableModem.SoftwareOperStatus
     - Device.X_LGI-COM_CableModem.EnergyMgtOperStatus
     - Device.X_LGI-COM_CableModem.OvertempProtModeState
     - Device.X_LGI-COM_CableModem.PartServiceFallback20
     - Device.X_LGI-COM_CableModem.DownstreamPrimaryChannelID
     - Device.X_LGI-COM_CableModem.QosServiceFlow.UpstreamNumberOfEntries
     - Device.X_LGI-COM_CableModem.QosServiceFlow.DownstreamNumberOfEntries
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.QosParamSetNumberOfEntries
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.QosParamSet.{i}.MaxTrafficRate
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.QosParamSet.{i}.MaxTrafficBurst
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.QosParamSet.{i}.MinReservedRate
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.QosParamSet.{i}.MaxConcatBurst
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.QosParamSet.{i}.SchedulingType
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.QosParamSetNumberOfEntries
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.QosParamSet.{i}.MaxTrafficRate
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.QosParamSet.{i}.MaxTrafficBurst
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.QosParamSet.{i}.MinReservedRate
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.QosParamSet.{i}.MaxConcatBurst
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.QosParamSet.{i}.SchedulingType
    
    TESTING PERFORMED:
    
    - Verified CM operating status, ToD status, Energy management mode with ACS
    - Verified whether the CM is currently operating in over temperature protected mode with ACS
    - Verified partial service fallback enable/disable status with ACS
    - Verified CM software operation status with ACS
    - Verified QoS service flow details with ACS
    - Verified CM registration status with ACS
    - Verified CM T1 timeout with ACS
    - Verified Downstream primary channel ID with ACS
    - Verified X_LGI-COM_CableModem object using dmcli
    
    Signed-off-by: XXXX <XXXX@YYYY.com>
    
    drop dependency on Puma 6 specific status.h header
    
    Temporary solution. Code added to ccsp-cm-agent is currently
    bypassing the CH HAL abstraction and using Intel specific types (and
    functions exported by the HAL but not defined in the HAL API). More
    cleanup is required, but this should be enough to at least build the
    code without a direct dependency on the Puma 6 SDK.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>
    
    update CosaDmlGiGetCmDoc30SwRegistrationState() to reflect recent cm_hal changes
    
    The docsis_getCmDoc30SwRegistrationState() CM HAL function now
    returns the correct registration state directly so the previous
    mapping can be removed.
    
    Include cm_hal.h in cosa_lgi_cablemodem_apis.c to define CmRegState_t
    (it should have been included before, but missing it has only caused
    compiler warnings until now).
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>
    
    check CM HAL function status against the correct definition
    
    CM HAL functions return results of RETURN_OK and RETURN_ERR. Check
    for those specific values, rather than making the assumption that
    they map directly to other similar definitions such as STATUS_OK or
    ANSC_STATUS_SUCCESS.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

---
+++ source/TR-181/middle_layer_src/cosa_lgi_cablemodem_internal.c
@@ -0,0 +1,299 @@
+/*********************************************************************
+ * Copyright 2017-2019 ARRIS Enterprises, LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **********************************************************************/
+
+#include "cosa_lgi_cablemodem_internal.h"
+
+PCOSA_DATAMODEL_LGI_CABLEMODEM g_pLgiCM;
+PCOSA_DATAMODEL_LGI_CABLEMODEM_QOS Lgi_QosSF;
+
+/**********************************************************************
+
+    caller:     owner of the object
+
+    prototype:
+
+        ANSC_HANDLE
+        CosaLgiCableModemCreate
+            (
+            );
+
+    description:
+
+        This function constructs cosa LgiCableModem object and return handle.
+
+    argument:
+
+    return:     newly created LgiCableModem object.
+
+**********************************************************************/
+
+ANSC_HANDLE
+CosaLgiCableModemCreate
+    (
+        VOID
+    )
+{
+    PCOSA_DATAMODEL_LGI_CABLEMODEM  pMyObject = NULL;
+
+    /*
+     * We create object by first allocating memory for holding the variables and member functions.
+     */
+    pMyObject = (PCOSA_DATAMODEL_LGI_CABLEMODEM)AnscAllocateMemory(sizeof(COSA_DATAMODEL_LGI_CABLEMODEM));
+
+    if ( !pMyObject )
+    {
+        return  (ANSC_HANDLE)NULL;
+    }
+
+    /*
+     * Initialize the common variables and functions for a container object.
+     */
+    pMyObject->Oid               = COSA_DATAMODEL_LGI_CABLEMODEM_OID;
+    pMyObject->Create            = CosaLgiCableModemCreate;
+    pMyObject->Remove            = CosaLgiCableModemRemove;
+    pMyObject->Initialize        = CosaLgiCableModemInitialize;
+
+    pMyObject->Initialize   ((ANSC_HANDLE)pMyObject);
+
+    g_pLgiCM = pMyObject;
+
+    return  (ANSC_HANDLE)pMyObject;
+}
+
+/**********************************************************************
+
+    caller:     self
+
+    prototype:
+
+        ANSC_STATUS
+        CosaLgiCableModemInitialize
+            (
+                ANSC_HANDLE                 hThisObject
+            );
+
+    description:
+
+        This function initiate  cosa LgiCableModem object and return handle.
+
+    argument:   ANSC_HANDLE                 hThisObject
+            This handle is actually the pointer of this object
+            itself.
+
+    return:     operation status.
+
+**********************************************************************/
+
+ANSC_STATUS
+CosaLgiCableModemInitialize
+    (
+        ANSC_HANDLE                 hThisObject
+    )
+{
+    return ANSC_STATUS_SUCCESS;
+}
+
+/**********************************************************************
+
+    caller:     self
+
+    prototype:
+
+        ANSC_STATUS
+        CosaLgiCableModemRemove
+            (
+                ANSC_HANDLE                 hThisObject
+            );
+
+    description:
+
+        This function initiate  cosa LgiCableModem object and return handle.
+
+    argument:   ANSC_HANDLE                 hThisObject
+            This handle is actually the pointer of this object
+            itself.
+
+    return:     operation status.
+
+**********************************************************************/
+
+ANSC_STATUS
+CosaLgiCableModemRemove
+    (
+        ANSC_HANDLE                 hThisObject
+    )
+{
+    PCOSA_DATAMODEL_LGI_CABLEMODEM     pMyObject    = (PCOSA_DATAMODEL_LGI_CABLEMODEM)hThisObject;
+
+    g_pLgiCM = NULL;
+
+    if(NULL != pMyObject)
+    {
+        /* Remove self */
+        AnscFreeMemory((ANSC_HANDLE)pMyObject);
+    }
+
+    return ANSC_STATUS_SUCCESS;
+}
+
+/**********************************************************************
+
+    caller:     owner of the object
+
+    prototype:
+
+        ANSC_HANDLE
+        CosaLgiCableModemQosCreate
+            (
+            );
+
+    description:
+
+        This function constructs cosa LgiCableModem object and return handle.
+
+    argument:
+
+    return:     newly created LgiCableModem object.
+
+**********************************************************************/
+ANSC_HANDLE
+CosaLgiCableModemQosCreate
+    (
+        VOID
+    )
+{
+    PCOSA_DATAMODEL_LGI_CABLEMODEM_QOS  pMyObject = NULL;
+
+    /*
+     * We create object by first allocating memory for holding the variables and member functions.
+     */
+    pMyObject = (PCOSA_DATAMODEL_LGI_CABLEMODEM_QOS)AnscAllocateMemory(sizeof(COSA_DATAMODEL_LGI_CABLEMODEM_QOS));
+
+    if ( !pMyObject )
+    {
+        return  (ANSC_HANDLE)NULL;
+    }
+
+    /*
+     * Initialize the common variables and functions for a container object.
+     */
+    pMyObject->Oid               = COSA_DATAMODEL_LGI_CABLEMODEM_OID;
+    pMyObject->Create            = CosaLgiCableModemQosCreate;
+    pMyObject->Remove            = CosaLgiCableModemQosRemove;
+    pMyObject->Initialize        = CosaLgiCableModemQosInitialize;
+
+    pMyObject->Initialize   ((ANSC_HANDLE)pMyObject);
+
+    Lgi_QosSF = pMyObject;
+
+    return  (ANSC_HANDLE)pMyObject;
+}
+
+/**********************************************************************
+
+    caller:     self
+
+    prototype:
+
+        ANSC_STATUS
+        CosaLgiCableModemQosInitialize
+            (
+                ANSC_HANDLE                 hThisObject
+            );
+
+    description:
+
+        This function initiate  cosa LgiCableModem object and return handle.
+
+    argument:   ANSC_HANDLE                 hThisObject
+            This handle is actually the pointer of this object
+            itself.
+
+    return:     operation status.
+
+**********************************************************************/
+
+ANSC_STATUS
+CosaLgiCableModemQosInitialize
+    (
+        ANSC_HANDLE                 hThisObject
+    )
+{
+    return ANSC_STATUS_SUCCESS;
+}
+
+/**********************************************************************
+
+    caller:     self
+
+    prototype:
+
+        ANSC_STATUS
+        CosaLgiCableModemQosRemove
+            (
+                ANSC_HANDLE                 hThisObject
+            );
+
+    description:
+
+        This function initiate  cosa LgiCableModem object and return handle.
+
+    argument:   ANSC_HANDLE                 hThisObject
+            This handle is actually the pointer of this object
+            itself.
+
+    return:     operation status.
+
+**********************************************************************/
+
+ANSC_STATUS
+CosaLgiCableModemQosRemove
+    (
+        ANSC_HANDLE                 hThisObject
+    )
+{
+    PCOSA_DATAMODEL_LGI_CABLEMODEM_QOS  pMyObject = (PCOSA_DATAMODEL_LGI_CABLEMODEM_QOS)hThisObject;
+
+    if ( NULL != pMyObject->pServiceUsFlowChannel )
+    {
+        AnscFreeMemory((ANSC_HANDLE)pMyObject->pServiceUsFlowChannel);
+    }
+
+    if ( NULL != pMyObject->pServiceDsFlowChannel )
+    {
+        AnscFreeMemory((ANSC_HANDLE)pMyObject->pServiceDsFlowChannel);
+    }
+
+    if ( NULL != pMyObject->pSfUsParamset )
+    {
+        AnscFreeMemory((ANSC_HANDLE)pMyObject->pSfUsParamset);
+    }
+
+    if ( NULL != pMyObject->pSfDsParamset )
+    {
+        AnscFreeMemory((ANSC_HANDLE)pMyObject->pSfDsParamset);
+    }
+
+    /* Remove self */
+    if (NULL != pMyObject)
+    {
+        AnscFreeMemory((ANSC_HANDLE)pMyObject);
+    }
+
+    Lgi_QosSF = NULL;
+    return ANSC_STATUS_SUCCESS;
+}
+
