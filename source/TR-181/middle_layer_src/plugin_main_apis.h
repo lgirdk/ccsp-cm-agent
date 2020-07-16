90f2f9ccb327b1baf6d30764d5071d71b47391da
Anu Chacko <achacko.contractor@libertyglobal.com>
2020-07-16 18:11:46 +0530

    Mercury V2 Connect Cloud support - Cable Modem Parameters ( RDKB-4477 : CR015 )
    Description:
    - Added TR-181 DML parameters of X_LGI-COM_CableModem object
    - Added implementation of X_LGI-COM_CableModem parameters
     - Device.X_LGI-COM_CableModem.CMStatus
     - Device.X_LGI-COM_CableModem.ToDStatus
     - Device.X_LGI-COM_CableModem.CmDoc30SwRegistrationState
     - Device.X_LGI-COM_CableModem.T1Timeouts
     - Device.X_LGI-COM_CableModem.SoftwareOperStatus
     - Device.X_LGI-COM_CableModem.EnergyMgtOperStatus
     - Device.X_LGI-COM_CableModem.OvertempProtModeState
     - Device.X_LGI-COM_CableModem.PartServiceFallback20
     - Device.X_LGI-COM_CableModem.DownstreamPrimaryChannelID
     - Device.X_LGI-COM_CableModem.QosServiceFlow.UpstreamNumberOfEntries
     - Device.X_LGI-COM_CableModem.QosServiceFlow.DownstreamNumberOfEntries
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.QosParamSetNumberOfEntries
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.QosParamSet.{i}.MaxTrafficRate
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.QosParamSet.{i}.MaxTrafficBurst
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.QosParamSet.{i}.MinReservedRate
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.QosParamSet.{i}.MaxConcatBurst
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.QosParamSet.{i}.SchedulingType
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.QosParamSetNumberOfEntries
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.QosParamSet.{i}.MaxTrafficRate
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.QosParamSet.{i}.MaxTrafficBurst
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.QosParamSet.{i}.MinReservedRate
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.QosParamSet.{i}.MaxConcatBurst
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.QosParamSet.{i}.SchedulingType
    
    TESTING PERFORMED:
    
    - Verified CM operating status, ToD status, Energy management mode with ACS
    - Verified whether the CM is currently operating in over temperature protected mode with ACS
    - Verified partial service fallback enable/disable status with ACS
    - Verified CM software operation status with ACS
    - Verified QoS service flow details with ACS
    - Verified CM registration status with ACS
    - Verified CM T1 timeout with ACS
    - Verified Downstream primary channel ID with ACS
    - Verified X_LGI-COM_CableModem object using dmcli

--- source/TR-181/middle_layer_src/plugin_main_apis.h
+++ source/TR-181/middle_layer_src/plugin_main_apis.h
@@ -118,6 +118,7 @@ extern COSARegisterCallBackAfterInitDmlProc  g_RegisterCallBackAfterInitDml;
 #define COSA_DATAMODEL_BASE_OID                                 0
 #define COSA_DATAMODEL_CM_OID                                   32
 #define COSA_DATAMODEL_RDKCENTRAL_CM_OID                        42
+#define COSA_DATAMODEL_LGI_CABLEMODEM_OID                       125
 
 /*
  * This is the cosa datamodel backend manager which is used to manager all backend object
@@ -127,7 +128,9 @@ extern COSARegisterCallBackAfterInitDmlProc  g_RegisterCallBackAfterInitDml;
     COSA_BASE_CONTENT                                                                       \
     ANSC_HANDLE                  hCM;                                                       \
     ANSC_HANDLE                  hDI;                                                       \
-    ANSC_HANDLE                  hRDKCM;                                                       \    
+    ANSC_HANDLE                  hRDKCM;                                                    \
+    ANSC_HANDLE                  hLgiCableModem;                                            \
+    ANSC_HANDLE                  hLgiCMQos;                                                 \
     PCOSA_PLUGIN_INFO            hCosaPluginInfo;											
 
 typedef  struct
