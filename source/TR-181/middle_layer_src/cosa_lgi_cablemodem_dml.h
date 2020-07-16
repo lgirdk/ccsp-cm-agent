d2b170b1de9d23507a8110def8fd8c2d1626cee6
Anu Chacko <achacko.contractor@libertyglobal.com>
2020-07-16 18:11:46 +0530

    Mercury V2 Connect Cloud support - Cable Modem Parameters ( RDKB-4477 : CR015 )
    Description:
    - Added TR-181 DML parameters of X_LGI-COM_CableModem object
    - Added implementation of X_LGI-COM_CableModem parameters
     - Device.X_LGI-COM_CableModem.CMStatus
     - Device.X_LGI-COM_CableModem.ToDStatus
     - Device.X_LGI-COM_CableModem.CmDoc30SwRegistrationState
     - Device.X_LGI-COM_CableModem.T1Timeouts
     - Device.X_LGI-COM_CableModem.SoftwareOperStatus
     - Device.X_LGI-COM_CableModem.EnergyMgtOperStatus
     - Device.X_LGI-COM_CableModem.OvertempProtModeState
     - Device.X_LGI-COM_CableModem.PartServiceFallback20
     - Device.X_LGI-COM_CableModem.DownstreamPrimaryChannelID
     - Device.X_LGI-COM_CableModem.QosServiceFlow.UpstreamNumberOfEntries
     - Device.X_LGI-COM_CableModem.QosServiceFlow.DownstreamNumberOfEntries
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.QosParamSetNumberOfEntries
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.QosParamSet.{i}.MaxTrafficRate
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.QosParamSet.{i}.MaxTrafficBurst
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.QosParamSet.{i}.MinReservedRate
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.QosParamSet.{i}.MaxConcatBurst
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Upstream.{i}.QosParamSet.{i}.SchedulingType
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.QosParamSetNumberOfEntries
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.QosParamSet.{i}.MaxTrafficRate
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.QosParamSet.{i}.MaxTrafficBurst
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.QosParamSet.{i}.MinReservedRate
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.QosParamSet.{i}.MaxConcatBurst
     - Device.X_LGI-COM_CableModem.QosServiceFlow.Downstream.{i}.QosParamSet.{i}.SchedulingType
    
    TESTING PERFORMED:
    
    - Verified CM operating status, ToD status, Energy management mode with ACS
    - Verified whether the CM is currently operating in over temperature protected mode with ACS
    - Verified partial service fallback enable/disable status with ACS
    - Verified CM software operation status with ACS
    - Verified QoS service flow details with ACS
    - Verified CM registration status with ACS
    - Verified CM T1 timeout with ACS
    - Verified Downstream primary channel ID with ACS
    - Verified X_LGI-COM_CableModem object using dmcli
    
    Signed-off-by: XXXX <XXXX@YYYY.com>
    
    drop dependency on Puma 6 specific status.h header
    
    Temporary solution. Code added to ccsp-cm-agent is currently
    bypassing the CH HAL abstraction and using Intel specific types (and
    functions exported by the HAL but not defined in the HAL API). More
    cleanup is required, but this should be enough to at least build the
    code without a direct dependency on the Puma 6 SDK.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>
    
    update CosaDmlGiGetCmDoc30SwRegistrationState() to reflect recent cm_hal changes
    
    The docsis_getCmDoc30SwRegistrationState() CM HAL function now
    returns the correct registration state directly so the previous
    mapping can be removed.
    
    Include cm_hal.h in cosa_lgi_cablemodem_apis.c to define CmRegState_t
    (it should have been included before, but missing it has only caused
    compiler warnings until now).
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>
    
    check CM HAL function status against the correct definition
    
    CM HAL functions return results of RETURN_OK and RETURN_ERR. Check
    for those specific values, rather than making the assumption that
    they map directly to other similar definitions such as STATUS_OK or
    ANSC_STATUS_SUCCESS.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

---
+++ source/TR-181/middle_layer_src/cosa_lgi_cablemodem_dml.h
@@ -0,0 +1,221 @@
+/*********************************************************************
+ * Copyright 2017-2019 ARRIS Enterprises, LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **********************************************************************/
+
+#ifndef  _COSA_LGI_CABLEMODEM_DML_H
+#define  _COSA_LGI_CABLEMODEM_DML_H
+
+#include "cosa_lgi_cablemodem_apis.h"
+
+/***********************************************************************
+
+ APIs for Object:
+
+    CableModemInfo.
+
+    *  LgiCableModem_GetParamBoolValue
+    *  LgiCableModem_GetParamIntValue
+    *  LgiCableModem_GetParamUlongValue
+    *  LgiCableModem_GetParamStringValue
+
+***********************************************************************/
+
+/**************************************************************************/
+/*                  Types of service flows                                */
+/**************************************************************************/
+#define SF_RESERVED                0
+#define SF_TYPE_UNDEFINED          1
+#define SF_BEST_EFFORT             2  /* best effort */
+#define SF_NON_REALTIME_PS         3  /* non real time polling service */
+#define SF_REALTIME_PL             4  /* real time polling service */
+#define SF_UNSOLICIT_GRANT_AD      5  /* unsolicited grant service with AD */
+#define SF_UNSOLICIT_GRANT         6  /* unsolicited grant service */
+#define SF_NOT_APPLICABLE          256
+
+/***************************************************************************/
+
+BOOL
+LgiCableModem_GetParamBoolValue
+    (
+        ANSC_HANDLE                 hInsContext,
+        char*                       ParamName,
+        BOOL*                       pBool
+    );
+
+BOOL
+LgiCableModem_GetParamIntValue
+    (
+        ANSC_HANDLE                 hInsContext,
+        char*                       ParamName,
+        int*                        pInt
+    );
+
+BOOL
+LgiCableModem_GetParamUlongValue
+    (
+        ANSC_HANDLE                 hInsContext,
+        char*                       ParamName,
+        ULONG*                      pUlong
+    );
+
+ULONG
+LgiCableModem_GetParamStringValue
+    (
+        ANSC_HANDLE                 hInsContext,
+        char*                       ParamName,
+        char*                       pValue,
+        ULONG*                      pUlSize
+    );
+
+BOOL
+LgiCableModem_SetParamBoolValue
+    (
+        ANSC_HANDLE                 hInsContext,
+        char*                       ParamName,
+        BOOL                        pBool
+    );
+
+BOOL
+LgiCableModem_SetParamUlongValue
+    (
+        ANSC_HANDLE                 hInsContext,
+        char*                       ParamName,
+        ULONG                       vUlong
+    );
+
+BOOL
+LgiCableModem_Validate
+    (
+        ANSC_HANDLE                 hInsContext,
+        char*                       pReturnParamName,
+        ULONG*                      puLength
+  );
+
+ULONG
+LgiCableModem_Commit
+    (
+        ANSC_HANDLE                 hInsContext
+    );
+
+ULONG
+LgiCableModem_Rollback
+    (
+        ANSC_HANDLE                 hInsContext
+    );
+
+
+/* Upstream Service */
+ULONG
+Lgi_US_QosServiceFlow_Synchronize
+    (
+        ANSC_HANDLE                 hInsContext
+    );
+
+BOOL
+Lgi_US_QosServiceFlow_IsUpdated
+    (
+        ANSC_HANDLE                 hInsContext
+    );
+
+ULONG
+Lgi_US_QosServiceFlow_GetEntryCount
+    (
+        ANSC_HANDLE                 hInsContext
+    );
+
+ANSC_HANDLE
+Lgi_US_QosServiceFlow_GetEntry
+    (
+        ANSC_HANDLE                 hInsContext,
+        ULONG                       nIndex,
+        ULONG*                      pInsNumber
+    );
+
+//Paramset API's
+ULONG
+Lgi_US_QosParamSet_GetEntryCount
+    (
+         ANSC_HANDLE                 hInsContext
+    );
+
+ANSC_HANDLE
+Lgi_US_QosParamSet_GetEntry
+    (
+        ANSC_HANDLE                 hInsContext,
+        ULONG                       nIndex,
+        ULONG*                      pInsNumber
+    );
+
+/* Downstream Service */
+ULONG
+Lgi_DS_QosServiceFlow_Synchronize
+    (
+        ANSC_HANDLE                 hInsContext
+    );
+
+BOOL
+Lgi_DS_QosServiceFlow_IsUpdated
+    (
+        ANSC_HANDLE                 hInsContext
+    );
+
+ULONG
+Lgi_DS_QosServiceFlow_GetEntryCount
+    (
+        ANSC_HANDLE                 hInsContext
+    );
+
+ANSC_HANDLE
+Lgi_DS_QosServiceFlow_GetEntry
+    (
+        ANSC_HANDLE                 hInsContext,
+        ULONG                       nIndex,
+        ULONG*                      pInsNumber
+    );
+
+//Paramset API's
+ULONG
+Lgi_DS_QosParamSet_GetEntryCount
+    (
+         ANSC_HANDLE                 hInsContext
+    );
+
+ANSC_HANDLE
+Lgi_DS_QosParamSet_GetEntry
+    (
+        ANSC_HANDLE                 hInsContext,
+        ULONG                       nIndex,
+        ULONG*                      pInsNumber
+    );
+
+/* Comman GET param API's for Upstream and Downstream Service */
+BOOL
+Lgi_QosServiceFlow_GetParamUlongValue
+    (
+        ANSC_HANDLE                 hInsContext,
+        char*                       ParamName,
+        ULONG*                      puLong
+    );
+
+BOOL
+Lgi_QosParamSet_GetParamUlongValue
+    (
+        ANSC_HANDLE                 hInsContext,
+        char*                       ParamName,
+        ULONG*                      puLong
+    );
+
+#endif
+
