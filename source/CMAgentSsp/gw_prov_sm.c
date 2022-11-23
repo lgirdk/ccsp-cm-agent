fbfa186b751934f8077de0fc33757ec6417b1296
Andre McCurdy <armccurdy@gmail.com>
2022-11-23 00:29:56 -0800

    CMAgentSsp: drop dependency on cimplog ( use rdk-logger version of onboarding logging )
    Align with gwprovapp.
    
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/CMAgentSsp/gw_prov_sm.c
+++ source/CMAgentSsp/gw_prov_sm.c
@@ -101,9 +101,8 @@
 #endif
 
 #ifdef FEATURE_SUPPORT_ONBOARD_LOGGING
-#include <cimplog/cimplog.h>
 #define LOGGING_MODULE           "GWPROV"
-#define OnboardLog(...)          onboarding_log(LOGGING_MODULE, __VA_ARGS__)
+#define OnboardLog(...)          rdk_log_onboard(LOGGING_MODULE, __VA_ARGS__)
 #else
 #define OnboardLog(...)
 #endif
