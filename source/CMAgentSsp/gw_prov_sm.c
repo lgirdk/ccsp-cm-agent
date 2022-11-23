d6e5249f1c59c07bc019db49e3c33c9487f4fc80
Andre McCurdy <armccurdy@gmail.com>
2022-11-23 14:42:35 -0800

    CMAgentSsp: make local variables static etc
    Signed-off-by: Andre McCurdy <armccurdy@gmail.com>

--- source/CMAgentSsp/gw_prov_sm.c
+++ source/CMAgentSsp/gw_prov_sm.c
@@ -261,7 +261,7 @@ typedef struct
     eGwpThreadType mType;       
 } GwpThread_MsgItem;
 
-GwpThread_MsgItem gwpthreadMsgArr[] = {
+static const GwpThread_MsgItem gwpthreadMsgArr[] = {
     {"erouter_mode",                               EROUTER_MODE},
     {"ipv4-status",                                IPV4STATUS},
     {"ipv6-status",                                IPV6STATUS},
@@ -408,7 +408,7 @@ static bool IsEthWanEnabled (void)
     return FALSE;
 }
 
-eGwpThreadType Get_GwpThreadType(char *name)
+static eGwpThreadType Get_GwpThreadType(char *name)
 {
     errno_t rc       = -1;
     int     ind      = -1;
