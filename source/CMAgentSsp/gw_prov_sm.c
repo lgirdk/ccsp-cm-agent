ebc6b8a54d4a922a90f5c9483269b8750922f465
Jyoti <jjyoti@libertyglobal.com>
2023-05-31 18:01:09 +0200

    DUT gets erouter ipv4 in bridge mode, when moving from erouter mode to bridge mode using config file ( OFW-4323 )
    [Description]
    NA
    
    [Solution]
    Before setting phy status of wan manager UP (Device.X_RDK_WanManager.CPEInterface.1.Phy.Status),
    CM Agent now waits for the TLV parsing to complete to progress on gateway initialization.
    
    [TESTING PERFORMED]
    Scenario 1 :
    - Move from erouter mode to bridge mode using config file, check DUT gets no erouter ipv4 in bridge mode.
    
    Scenario 2 :
    - DUT is in erouter mode and docsis cable is connected.
    - CM agents wait for few secs for tlv-status sysevent after receiving EVENT_GWP_LINK_UP event in GWP_EventHandler().
    - erouter get the ip
    - Disconnect the docsis cable, GWP_EventHandler() receive EVENT_GWP_LINK_DOWN2 event and does not wait for TLV202-status sysevent as it already set to success.
    
    Scenario 3 :
    - Disconnect the Docsis cable.
    - Reboot the device in erouter mode.
    - No EVENT_GWP_LINK_DOWN2 event receive in GWP_EventHandler() function at the bootup time.
    - Connect the docsis cable.
    - CM agents wait for few secs for tlv-status sysevent after receiving EVENT_GWP_LINK_UP event in GWP_EventHandler().
    - erouter get the ip.
    
    Signed-off-by: Jyoti <jjyoti@libertyglobal.com>

--- source/CMAgentSsp/gw_prov_sm.c
+++ source/CMAgentSsp/gw_prov_sm.c
@@ -419,6 +419,11 @@ typedef struct{
 docsis_linkdow_testcase DocsisLd_cfg={false,false};
 #endif //WAN_FAILOVER_SUPPORTED
 
+#if (_LG_MV2_PLUS_)
+// Variable to track if tlv parsing is successful.
+static int tlv_flag;
+#endif
+
 /**************************************************************************/
 /*      LOCAL FUNCTIONS:                                                  */
 /**************************************************************************/
@@ -3539,6 +3544,9 @@ gimReply:
     {
         //Notifying the CcspPandM and CcspTr069 module that the TLV parsing is successful and done
         sysevent_set(sysevent_fd_gs, sysevent_token_gs, "TLV202-status", "success", 0);
+#if (_LG_MV2_PLUS_)
+        tlv_flag = 1;
+#endif
     }
     else
     {
@@ -4174,6 +4182,13 @@ void *GWP_EventHandler(void *arg)
             sleep(1);
         }
 
+#if (_LG_MV2_PLUS_)
+        while(tlv_flag == 0)
+        {
+            GWPROV_PRINT("%s:%d: Waiting for ccsp-cm-agent to parse TLV config file\n", __FUNCTION__, __LINE__);
+            sleep(1);//sleep(1) is to avoid lots of trace msgs when there is latency
+        }
+#endif
 
         GWPROV_PRINT("%s:%d: going to wait in mq receive \n", __FUNCTION__, __LINE__);
         /* receive the message */
