90d3ab0d52434522cc1edadfaa0093fa30cb497a
Devin Mungan <dmungan@libertyglobal.com>
2023-09-06 11:19:02 +0200

    fix IPv4 internet connectivity fails when erouter mode is switched from dslite mode to dual mode using config file ( OFW-5005 F3896SI-2221 )
    [Issue]
    cm-agent issues a wan-restart after the erouter mode change is triggered via bootcfg file.
    wan-restart deletes the "current_wan_ipaddr" sysevent but fails to rewrite it and trigger a firewall
    restart. if the corresponding sysevent is empty, lan2wan communication will be blocked by a rule.
    
    [Solution]
    remove wan-restart trigger from cm-agent after bootcfg file is received. In theory, this removal shall cause
    no side effects because in current implementation, wanManager will only start initializing the docsis interface
    after the bootcfg file is parsed and Phy.Status=Up is set by the CM agent.
    
    [Testing Performed]
    1. FR the DUT and make sure the bootcfg file is configured to DSLite mode.
    2. Wait for the bootup to complete and verify ipv6 connectivity is present.
    3. Change the bootcfg file to Dual mode and restart the DUT.
    4. Wait for bootup to complete and verify that current_wan_ipaddr is the
       same as erouter0 ipv4 address.
    5. Verify that the lan client has ipv4 connectivity.
    
    Signed-off-by: Devin Mungan(dmungan@libertyglobal.com)

--- source/CMAgentSsp/gw_prov_sm.c
+++ source/CMAgentSsp/gw_prov_sm.c
@@ -1808,7 +1808,6 @@ void GWP_UpdateERouterMode(void)
 #endif
                 if(!once)
                     check_lan_wan_ready();
-                v_secure_system("sysevent set erouter_mode-updated");
             }
         }
 #if defined(WAN_MANAGER_UNIFICATION_ENABLED)
