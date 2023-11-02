d9dca80153149603828c528f7d8df224ec86a13f
Devin Mungan <dmungan@libertyglobal.com>
2023-11-02 12:16:21 +0100

    Revert "dual wan: fix docsis link event are not parsed when ethwan is enabled ( OFW-3605 )" ( OFW-5250 )
    Legacy development on Wan Failover is no longer needed.
    
    Signed-off-by: Devin Mungan(dmungan@libertyglobal.com)

--- source/CMAgentSsp/gw_prov_sm.c
+++ source/CMAgentSsp/gw_prov_sm.c
@@ -2792,6 +2792,10 @@ static void GWP_act_DocsisLinkDown_callback_1()
 	}
 #endif
 
+    if (IsEthWanEnabled() == true)
+    {
+        return;
+    }
     phylink_wan_state = 0;
 	GWPROV_PRINT(" Entry %s \n", __FUNCTION__);
     sysevent_set(sysevent_fd_gs, sysevent_token_gs, "phylink_wan_state", "down", 0);
@@ -2815,6 +2819,11 @@ static void GWP_act_DocsisLinkDown_callback_2()
 	}
 #endif
 
+    if (IsEthWanEnabled() == true)
+    {
+        return;
+    }
+ 
     GWPROV_PRINT("Entry %s \n",__FUNCTION__);
     if (eRouterMode != DOCESAFE_ENABLE_DISABLE_extIf)
     {
@@ -2966,6 +2975,11 @@ static int GWP_act_DocsisLinkUp_callback()
 		return 0;
 	}
 #endif
+
+    if (IsEthWanEnabled() == true)
+    {
+        return -1;
+    }
 
     phylink_wan_state = 1;
     GWPROV_PRINT("Entry %s \n",__FUNCTION__);
